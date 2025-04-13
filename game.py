eb96753dd546b6386b0954463c74eafb6e628664
Berihuete Nunez, Engel <mastering23@gmail.com>
2025-04-13 13:25:58 -0400

    select player | current Turn player to attack

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
+import random
 class Character:
 
-  def __init__(self,name,guild,power,block,specialAttack,level, attackLevel):
+  def __init__(self, name, guild, power, block, specialAttack, level, attackLevel):
     self.name = name
     self.guild = guild
     self.power = power
@@ -14,8 +15,6 @@ class Character:
     print(f"Attack : {self.power} {self.attackLevel}")
     print("-----------------------------------\n")
 
-
-
   def SpecialAttack(self):
     print(f"Player : {self.name}")
     print(f"Attack : {self.specialAttack}")
@@ -28,16 +27,37 @@ class Character:
     print("-----------------------------------\n")
 
 
-
-
-player1 = Character("Max Flame","Red Guild","FireBall","Cross Block","Fire Thunder",100,20)
-player2 = Character("FoxShadow","Blue Guild","Ice punch ","Ice Shield","Cold Ghost",100,20)
+player1 = Character("Max Flame", "Red Guild", "FireBall", "Cross Block", "Fire Thunder", 100, 20)
+player2 = Character("FoxShadow", "Blue Guild", "Ice punch ", "Ice Shield", "Cold Ghost", 100, 20)
 print("---TeSting Game Fight---\n")
-player1.Attack()
-player2.Block()
-player1.Attack()
-player2.Block()
-player2.Attack()
-player1.Block()
-player2.SpecialAttack()
-player1.Block()
+
+
+# select player
+def SelectPlayer():
+  title = " Game  |  Python Kai \n"
+  selectPlayerMessage = "Select player : "
+  print(f"{title} {selectPlayerMessage}")
+  op = int(input("type:"))
+  if op == 1:
+    return player1.name
+  elif op == 2:
+    return player2.name
+
+def currentTurn(playerSelected):
+  # select who go first
+  currentTurnSelected  = random.randint(0,1)
+  print(currentTurnSelected)
+
+playerSelected = SelectPlayer()
+print(playerSelected)
+currentTurn(playerSelected)
+
+
+# player1.Attack()
+# player2.Block()
+# player1.Attack()
+# player2.Block()
+# player2.Attack()
+# player1.Block()
+# player2.SpecialAttack()
+# player1.Block()
